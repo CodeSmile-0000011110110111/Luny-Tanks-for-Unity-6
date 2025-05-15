765470333d423ba184521d8f9735056dd294761e
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-05-15 19:41:25 +0200

    update default modules and context

---
+++ Assets/Luny/Editor/UnityEditorCoreModule (GENERATED)/_UnityEditor_LuaModule_Loader.cs
@@ -0,0 +1,20 @@
+// --------------------------------------------------------------
+// GENERATED FILE ----> DO NOT EDIT <---- CHANGES WILL BE LOST !!
+// --------------------------------------------------------------
+using CodeSmile.Luny;
+using Lua;
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeSmileEditor.Luny.DefaultContext
+{
+	public sealed class _UnityEditor_LuaModule_Loader : LuaModuleLoader
+	{
+		public override void Load(LuaState luaState)
+		{
+base.Load(luaState);
+		}
+	}
+}
+
+
---
+++ Assets/Luny/Runtime/System.IO Module (GENERATED)/_System_IO_LuaModule_Loader.cs
@@ -0,0 +1,20 @@
+// --------------------------------------------------------------
+// GENERATED FILE ----> DO NOT EDIT <---- CHANGES WILL BE LOST !!
+// --------------------------------------------------------------
+using CodeSmile.Luny;
+using Lua;
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeSmile.Luny.DefaultContext
+{
+	public sealed class _System_IO_LuaModule_Loader : LuaModuleLoader
+	{
+		public override void Load(LuaState luaState)
+		{
+base.Load(luaState);
+		}
+	}
+}
+
+
